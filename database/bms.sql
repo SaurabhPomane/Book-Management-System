5a4e48c8591212c1edbe0cc2dd3611d1d5829cfc
SaurabhPomane <saurabhpomane5@gmail.com>
2024-09-18 13:00:27 +0530

    First commit

---
+++ database/bms.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE college;
+use college;
+CREATE TABLE STUDENT(
+id int primary key,
+name varchar(50),
+age int not null
+);
+insert inTO STUDENT VALUES(1,'saurabh',21);
+insert into STUDENT VALUES(2,'AMAN',23);
+
+SELECT * FROM college.student;
